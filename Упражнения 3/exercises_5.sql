947440cc854ddaf7c0cac8c6edb019ed6e16ec0c
Mitya <116806816+EnnerDA@users.noreply.github.com>
2023-02-02 12:14:43 +0500

    Create exercises_5.sql

---
+++ Упражнения 3/exercises_5.sql
@@ -0,0 +1,5 @@
+/* 5.1 Заполните в следующем запросе пробелы (обозначенные как <число>), чтобы получить такие результаты:
+mysql> SELECT e.emp_id, e.fname, e.lname, b.name 	
+     > FROM employee e INNER JOIN <1> b 	
+     >   ON e.assigned_branch_id = b.<2>;	
+*/
